47a3e1adfecb031deeb3d48c103a8ba3848b0864
Erdem <57351007+ErdemGns@users.noreply.github.com>
2021-01-13 21:56:56 +0300

    Update 1_a.py

--- 1_a.py
+++ 1_a.py
@@ -100,4 +100,5 @@ carpim = C_2 + C_4 + C_6
 # U değeri hesabı
 U = (GM / r) * (1 - carpim) + (pow(w, 2) / 2) * \
     pow(r, 2) * pow(math.sin(yeni_enlem), 2)
-print("U: ", U)
+
+print("U: %.4f m²/s²" % U)
