ab1c10cefd79e130559519af431ed5b926d46d08
Erdem <57351007+ErdemGns@users.noreply.github.com>
2021-01-18 21:23:41 +0300

    Update 3_a.py

--- 3_a.py
+++ 3_a.py
@@ -2,13 +2,9 @@
 import math
 
 # Verilen değerler
-G = 9.796232618985 * pow(10, 5) / 100000  # Gravite (7. değer) (Gal)
-
+W_0 = 62636856.0  # Referans potansiyel m²/s²
 W_P = 6.262129752474 * pow(10, 7)  # Gravite potansiyel (8. değer) m²/s²
-# Sabit değerler
 
 # Jeopotansiyel sayı (yükseklik) hesabı
-W_0 = 62636856.0  # Referans potansiyel m²/s²
 C_P = W_0 - W_P
 print("Jeopotansiyel yükseklik: %.4f" % C_P)
-
